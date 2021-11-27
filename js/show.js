71aaa0216d9aa7081d4be77c32e755f9226e3210
Anirban <89.anirbandey@gmail.com>
2021-11-27 13:40:40 +0530

    change

--- js/show.js
+++ js/show.js
@@ -5,11 +5,14 @@ $(document).ready(function(){
     console.log(objArr);
 
     var html="";
+    var cardHtml= "";
     $('#table-body').html("");
     for(var i=0;i<objArr.length;i++){
-        html = html + "<tr><th scope='row'>"+(i+1)+"</th><td>"+objArr[i].name+"</td><td>"+objArr[i].email+"</td><td>"+objArr[i].work+"</td><td><input type='file' id='fileUpload"+i+"' class='form-control' aria-label='file example' required></td></tr>";
+        html = html + "<tr><th scope='row'>"+(i+1)+"</th><td>"+objArr[i].name+"</td><td>"+objArr[i].email+"</td><td>"+objArr[i].work+"</td></tr>";
+        cardHtml = cardHtml + "<div class='card' style='width: 28rem;'><div class='card-body'><h5 class='card-title'>Candidate File Upload</h5><h6 class='card-subtitle mb-2 text-muted'>To be filled by candidate</h6><p class='card-text'><label for='basic-url' class='form-label'>Name: </label> "+objArr[i].name+"</p><p class='card-text'><label for='basic-url' class='form-label'>Email: </label> "+objArr[i].email+"</p><p class='card-text'> <label for='basic-url' class='form-label'>Work: </label> "+objArr[i].work+"</p><label for='basic-url' class='form-label'>File: </label><input type='file' id='fileUpload"+i+"' class='form-control' aria-label='file example' required></div></div>";
     }
     $('#table-body').append(html);
+    $('.space').append(cardHtml);
 });
 
 $('#finalSubmit').click(function(){
